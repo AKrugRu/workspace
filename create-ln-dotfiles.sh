ce2a2947ae7d7221ceb0e5a36cc587f34a646805
Aleksey <pokodim@gmail.com>
2025-06-29 19:23:18 +0300

    Статус бар перенесен вверх + fix пути tmux.conf

--- create-ln-dotfiles.sh
+++ create-ln-dotfiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ln -sf $(pwd)/dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
-ln -sf $(pwd)/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/dotfiles/tmux/.tmux.conf ~/.tmux.conf
